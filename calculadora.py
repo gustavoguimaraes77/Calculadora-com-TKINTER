2a693109f88b008d562a8d80323718dda9063836
Gustavo Guimarães <160160059+gustavoguimaraes77@users.noreply.github.com>
2024-04-10 16:26:38 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -29,7 +29,7 @@ def div():
 
 root = tk.Tk()
 root.title('CALCULADORA')
-root.geometry('300x300')
+root.geometry('300x410')
 
 label1 = tk.Label(root, text='1º número:')
 label1.pack()
@@ -53,14 +53,26 @@ label3.pack()
 btn_soma = tk.Button(root, text='Soma', width = 20, fg='white', bg='#000000', command = somar)
 btn_soma.pack()
 
+label4 = tk.Label(root, text=' ')
+label4.pack()
+
 btn_sub = tk.Button(root, text='Subtração', width = 20, fg='white', bg='#000000', command = subtrair)
 btn_sub.pack()
+
+label5 = tk.Label(root, text=' ')
+label5.pack()
 
 btn_multi = tk.Button(root, text='Multiplicação', width = 20, fg='white', bg='#000000', command = multip)
 btn_multi.pack()
 
+label6 = tk.Label(root, text=' ')
+label6.pack()
+
 btn_div = tk.Button(root, text='Divisão', width = 20, fg='white', bg='#000000', command = div)
 btn_div.pack()
+
+label7 = tk.Label(root, text=' ')
+label7.pack()
 
 result = tk.Label(root, text='Resultado:')
 result.pack()
